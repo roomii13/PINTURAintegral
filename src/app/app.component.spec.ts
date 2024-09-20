f5f7f1120d79371c83efb4d5f5a5173225b6b06b
roomii13 <71903262+roomii13@users.noreply.github.com>
2024-09-20 10:17:16 -0300

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'PinturaIntegral' title`, () => {
+  it(`should have the 'PinturaProfesional' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PinturaIntegral');
+    expect(app.title).toEqual('PinturaProfesional');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PinturaIntegral');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PinturaProfesional');
   });
 });
